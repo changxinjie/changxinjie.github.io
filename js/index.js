04ea940085ec3962183d1bc1db183b439d33eeaa
chang.xinjie <215920014@qq.com>
2016-06-11 00:59:23 +0800

    针对小屏幕,调整按钮间距

--- js/index.js
+++ js/index.js
@@ -6,17 +6,42 @@ app.controller("footCtrl", ['$scope', function($scope) {
 	$scope.tips = tips;
 	$scope.links = links;
 }]);
-app.controller("NavCtrl", ["$scope","Coms", function($scope,Coms) {
+app.controller("NavCtrl", ["$scope", "Coms", function($scope, Coms) {
 	$scope.menu = menu;
-	$scope.click = function(method,link) {
-		var deal = angular.bind(this, method, link,Coms);
+	$scope.click = function(method, link) {
+		var deal = angular.bind(this, method, link, Coms);
 		deal();
 	}
 }]);
-app.controller("MainCtrl", ["$scope","Coms", function($scope,Coms) {
+app.controller("MainCtrl", ["$scope", "Coms", function($scope, Coms) {
 	$scope.main = "html/introduce.html";
 	Coms.onSignal('openLink', function(event, data) {
 		console.log(data);
 		$scope.main = data;
 	}, $scope);
 }]);
+app.controller("endpointCtrl", ["$scope", function($scope) {
+	$scope.rate = 7;
+	$scope.max = 10;
+	$scope.isReadonly = false;
+
+	$scope.hoveringOver = function(value) {
+		$scope.overStar = value;
+		$scope.percent = 100 * (value / $scope.max);
+	};
+
+	$scope.ratingStates = [{
+		stateOn: 'glyphicon-ok-sign',
+		stateOff: 'glyphicon-ok-circle'
+	}, {
+		stateOn: 'glyphicon-star',
+		stateOff: 'glyphicon-star-empty'
+	}, {
+		stateOn: 'glyphicon-heart',
+		stateOff: 'glyphicon-ban-circle'
+	}, {
+		stateOn: 'glyphicon-heart'
+	}, {
+		stateOff: 'glyphicon-off'
+	}];
+}])
